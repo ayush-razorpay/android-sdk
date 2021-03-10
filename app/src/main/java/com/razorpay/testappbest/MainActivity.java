8978daf44fc8f1759697ff0023ffcd1fef809cca
Ayush Vipul <ayush.vipul@razorpay.com>
2021-03-10 21:10:24 +0530

    initial commit

---
+++ app/src/main/java/com/razorpay/testappbest/MainActivity.java
@@ -0,0 +1,65 @@
+package com.razorpay.testappbest;
+
+import android.app.Activity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.razorpay.Checkout;
+import org.json.JSONObject;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Checkout.preload(getApplicationContext());
+
+
+        Button button = (Button) findViewById(R.id.payButton);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startPayment();
+            }
+        });
+
+    }
+
+    public void startPayment() {
+        /*
+          You need to pass current activity in order to let Razorpay create CheckoutActivity
+         */
+        final Activity activity = this;
+
+        final Checkout co = new Checkout();
+        co.setKeyID("rzp_test_q4oG1kUXJsxHJb");
+        try {
+            JSONObject options = new JSONObject();
+            options.put("name", "Razorpay Corp");
+            options.put("description", "Demoing Charges");
+            //You can omit the image option to fetch the image from dashboard
+            options.put("image", "https://s3.amazonaws.com/rzp-mobile/images/rzp.png");
+           options.put("currency", "INR");
+          //  options.put("amount", "100");
+            options.put("order_id", "order_Gku3aYRezpSXjQ");
+
+            JSONObject preFill = new JSONObject();
+            preFill.put("email", "test@razorpay.com");
+            preFill.put("contact", "9876543210");
+
+            options.put("prefill", preFill);
+
+            co.open(activity, options);
+        } catch (Exception e) {
+            Toast.makeText(activity, "Error in payment: " + e.getMessage(), Toast.LENGTH_SHORT)
+                    .show();
+            e.printStackTrace();
+        }
+    }
+
+}
